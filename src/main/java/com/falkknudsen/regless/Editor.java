00df69aa06a21e2e16b7a96c3b7158652620decf
Louis <louis@falkknudsen.com>
2025-10-12 00:35:12 +0200

    Performed a lot of optimisations to the formatting. The method now takes a character array instead of a string, and appends bit by bit instead of with regular expressions and string copying. Strings are now all relegated to constant variables for easier maintainability. No strings are concatenated at run-time now.

--- src/main/java/com/falkknudsen/regless/Editor.java
+++ src/main/java/com/falkknudsen/regless/Editor.java
@@ -2,7 +2,8 @@ package com.falkknudsen.regless;
 
 import javafx.scene.Node;
 import javafx.scene.web.HTMLEditor;
-import org.jsoup.Jsoup;
+import org.jsoup.parser.HtmlTreeBuilder;
+import org.jsoup.parser.Parser;
 import org.jspecify.annotations.Nullable;
 
 import java.util.Set;
@@ -18,17 +19,18 @@ import static com.falkknudsen.regless.View.VERBOSE;
 public class Editor extends HTMLEditor {
     /// Cached string builder.
     private final StringBuilder sb = new StringBuilder(1000);
+    /// Cached string builder for formatting text as HTML.
+    private final StringBuilder _sb = new StringBuilder(1000);
 
-    /// The constructor removes the HTMLEditor's toolbars, and sets the contents
-    /// to have the right style.
+    /** The constructor removes the HTMLEditor's toolbars, and sets the contents to have the right
+    style. */
     public Editor() {
         Set<Node> nodes = this.lookupAll(".tool-bar");
         for (Node node : nodes) {
             node.setVisible(false);
             node.setManaged(false);
         }
-        setHtmlText(Preamble + Postamble);
-        //setStyle("-fx-background-color: transparent;");
+        setHtmlText(EmptyHTMLBody);
     }
 
     /// Returns the contents of the editor stripped of all tags.
@@ -37,99 +39,84 @@ public class Editor extends HTMLEditor {
         return Jsoup.parse(html).text();
     }
 
-    /** Formats the editor's underlying HTML text so that the background of text
-     that has been matched by a named subgroup is coloured. Different named
-     subgroups have different colours. */
-    public void format(@Nullable Matcher matcher, String text) {
-        if (matcher == null || !matcher.find() || matcher.pattern().toString().isEmpty()) {
+    /**
+     Formats the editor's underlying HTML text so that the background of text that has been matched
+     by a named subgroup is coloured. Different named subgroups have different colours.
+     */
+    public void format(@Nullable Matcher matcher, char[] text) {
+        if (matcher == null || !matcher.find() || text.length == 0
+            // this is cached, so it's just a few of function calls:
+            || matcher.pattern().toString().isEmpty()) {
+            setHtmlText(EmptyHTMLBody);
             return;
         }
-        System.out.println("Before: \n" + text);
-        sb.delete(0, sb.length())
-            .append(Preamble);
+
+        _sb.setLength(0);
+        _sb.append(Preamble);
         var currentIdx = 0;
         do {
             var leftIdx = matcher.start();
             var rightIdx = matcher.end();
+
+            // Characters before match:
             if (leftIdx > currentIdx) {
-                String str = text.substring(currentIdx, leftIdx)
-                    .replaceAll("[^\n\t]", "&nbsp;")
-                    .replaceAll("[\t]", "&nbsp;&nbsp;&nbsp;&nbsp;")
-                    .replaceAll("\n", "<br/>");
-
-                sb.append("<span>")
-                    .append(str)
-                    .append("</span>");
+                _sb.append(SPAN_START);
+                for (int i = currentIdx; i < leftIdx; i++) {
+                    _sb.append(replace(text[i]));
+                }
+                _sb.append(SPAN_END);
             }
 
+            // Characters for match:
             int group = getGroup(matcher, leftIdx, rightIdx);
-            String colour = nonCaptureColour;
             if (VERBOSE) System.out.println("Group nr: " + group);
-            if (group > 0) {
-                colour = BackgroundColours[group % BackgroundColours.length];
-            }
-            String str = text.substring(leftIdx, rightIdx)
-                .replaceAll("[^\n\t]", "&nbsp;")
-                .replaceAll("[\t]", "&nbsp;&nbsp;&nbsp;&nbsp;")
-                .replaceAll("\n", "<br/>");
 
-            sb.append("<span style=\"background-color:")
-                .append(colour)
-                .append(";\">")
-                .append(str)
-                .append("</span>");
+            if (group > 0) {
+                _sb.append(ColourSpans[group % ColourSpans.length]);
+            } else {
+                _sb.append(nonCaptureColourSpan);
+            }
+            for (int i = leftIdx; i < rightIdx; i++) {
+                _sb.append(replace(text[i]));
+            }
+            _sb.append(SPAN_END);
             currentIdx = rightIdx;
         } while (matcher.find());
 
-        if (currentIdx < text.length()) {
-            String str = text.substring(currentIdx)
-                .replaceAll("[^\n\t]", "&nbsp;")
-                .replaceAll("[\t]", "&nbsp;&nbsp;&nbsp;&nbsp;")
-                .replaceAll("\n", "<br/>");
-
-            sb.append("<span>")
-                .append(str)
-                .append("</span>");
+        // Characters after match:
+        if (currentIdx < text.length) {
+            _sb.append(SPAN_START);
+            for (int i = currentIdx; i < text.length; i++) {
+                _sb.append(replace(text[i]));
+            }
+            _sb.append(SPAN_END);
         }
-        sb.append(Postamble);
-        setHtmlText(sb.toString());
+        _sb.append(Postamble);
+        setHtmlText(_sb.toString());
         if (VERBOSE) System.out.println("After: \n" + _sb);
     }
 
-    /// Prefix to the contents of the HTMLEditor
-    private static final String Preamble =
-        "<html dir=\"ltr\">" +
-            "<head>" +
-                "<style>" +
-                    // Typeface set to one that is monospace.
-                    // This line height matches text-area's at this font size.
-                    "body {font-family:Consolas;font-size:16px;font-scale:1;line-height:21px;}" +
-                    // Erasing the three rules which define a default <p>
-                    "p {display:inline;margin-top:0;margin-bottom:0;}" +
-                "</style>" +
-            "</head>" +
-            // Has to be set to true every time in order for editor to continue being editable.
-            "<body contenteditable=\"true\">";
+    /// Return the whitespace HTML that will replace the given character.
+    private static String replace(char c) {
+        return switch (c) {
+            case '\t' -> "&nbsp;&nbsp;&nbsp;&nbsp;";
+            case '\n' -> "<br/>";
+            default -> "&nbsp;";
+        };
+    }
 
-    /// Suffix to the contents of the HTMLEditor
-    private static final String Postamble = "</body></html>";
-
-    /// Colours used for the text background. Each matching group is assigned one, round-robin style,
-    /// by a simple {@code BackgroundColours\[groupID % groups.length()]}
-    public static final String[] BackgroundColours = {
-            "#c4e8ac","#f6d7a6", "#c8c8ff", "#f2cfff", "#ffc5bf", "#c8e7d6"};
-
-    /// Colour used for text background when something was matched, but not by a capturing group.
-    private static final String nonCaptureColour = "#d5ebff";
-
-    /** Returns the group number of the named capturing group that matched most
-     recently.<br/>
-     Will <b>not</b> fail fast if {@link Matcher#hasMatch matcher.hasMatch()} is false.<br/>
-     Although the latter two parameters are unnecessary, they're needed outside
-     anyway, so we may as well reduce the number of extraneous function calls.
-     @param matcher The Matcher which found the match.
-     @param start The value of matcher.start()
-     @param end The value of matcher.end() */
+    /**
+     Returns the group number of the named capturing group that matched most recently.<br/> Will
+     <b>not</b> fail fast if {@link Matcher#hasMatch matcher.hasMatch()} is false.<br/> Although the
+     latter two parameters are unnecessary, they're needed outside anyway, so we may as well reduce
+     the number of extraneous function calls.
+     @param matcher
+     The Matcher which found the match.
+     @param start
+     The value of matcher.start()
+     @param end
+     The value of matcher.end()
+     */
     private static int getGroup(Matcher matcher, int start, int end) {
         int groupCount = matcher.groupCount();
         for (int i = 1; i < groupCount; i++) {
@@ -140,11 +127,61 @@ public class Editor extends HTMLEditor {
         return -1;
     }
 
-    /** Returns the group number of the named capturing group that matched most
-     recently.<br/>
-     Will <b>not</b> fail fast if {@link Matcher#hasMatch matcher.hasMatch()} is false.
-     @param matcher The Matcher which found the match. */
+    /**
+     Returns the group number of the named capturing group that matched most recently.<br/> Will
+     <b>not</b> fail fast if {@link Matcher#hasMatch matcher.hasMatch()} is false.
+     @param matcher
+     The Matcher which found the match.
+     */
     private static int getGroup(Matcher matcher) {
         return getGroup(matcher, matcher.start(), matcher.end());
+    }
+
+    //##########################################################################
+    // String literals/constants
+    //##########################################################################
+
+    private static final String SPAN_START = "<span>";
+    private static final String SPAN_END = "</span>";
+    private static final String SPAN_WITH_COLOUR_START = "<span style=\"background-color:";
+    private static final String SPAN_WITH_COLOUR_END = ";\">";
+
+    /// Prefix to the contents of the HTMLEditor
+    private static final String Preamble =
+            "<html dir=\"ltr\">" +
+            "<head>" +
+            "<style>" +
+            // Typeface set to one that is monospace.
+            // This line height matches text-area's at this font size.
+            "body{font-family:Consolas;font-size:16px;font-scale:1;line-height:21px;}" +
+            // Erasing the three rules which define a default <p>
+            "p{display:inline;margin-top:0;margin-bottom:0;}" +
+            "</style>" +
+            "</head>" +
+            // Has to be set to true every time in order for editor to continue being editable.
+            "<body contenteditable=\"true\">";
+
+    /// Suffix to the contents of the HTMLEditor
+    private static final String Postamble = "</body></html>";
+
+    private static final String EmptyHTMLBody = Preamble + Postamble;
+
+    /// Colours used for the text background. Each matching group is assigned one, round-robin
+    /// style, by a simple {@code BackgroundColours\[groupID % groups.length()]}
+    private static final String[] BackgroundColours = {
+            "#c4e8ac", "#f6d7a6", "#c8c8ff", "#f2cfff", "#ffc5bf", "#c8e7d6" };
+
+    /// Colour used for text background when something was matched, but not by a capturing group.
+    private static final String nonCaptureColour = "#d5ebff";
+    private static final String defaultBackground = "#ffffff";
+
+    private static final String[] ColourSpans = new String[BackgroundColours.length];
+    private static final String nonCaptureColourSpan =
+            SPAN_WITH_COLOUR_START + nonCaptureColour + SPAN_WITH_COLOUR_END;
+
+    static {
+        for (int i = 0; i < BackgroundColours.length; i++) {
+            ColourSpans[i] = SPAN_WITH_COLOUR_START + BackgroundColours[i] + SPAN_WITH_COLOUR_END;
+        }
     }
 }
